5847738bbf70abb82348270c5179b68f763014e4
Chris Dillon <milkfilk@gmail.com>
2011-01-13 10:56:32 -0500

    Rename README

--- object_id_inflection.rb
+++ object_id_inflection.rb
@@ -14,9 +14,6 @@ starting_number = 4000000000000000000
 # this will be the number we'll try with
 current_number = starting_number
 
-#
-starting_length = starting_number.to_s.length
-
 # our decimal position that will be used for the loop
 # we don't need to start with 4, which is starting_nmber[0..0]
 index = 1
@@ -47,6 +44,7 @@ end
 
 # go until we've iterated along the length of starting_number
 while (index <= starting_number.to_s.length - 1)
+  digit_done = false
   
   digit_at_index = current_number.to_s[index..index].to_i
 
@@ -78,9 +76,9 @@ while (index <= starting_number.to_s.length - 1)
   # if we start with 0, this becomes 5 if going up
   # if we start with 5, this becomes 3 if going down
   # it's a half to target number
+  
+  # increase
   if direction
-    puts "increase"
-    
     digit_at_index += jump unless digit_at_index == 9
     
     #puts digit_at_index
@@ -88,48 +86,39 @@ while (index <= starting_number.to_s.length - 1)
       digit_done = true
     else
       digit_done = false
-      
-      if changed_directions == 4
-        puts "INCREASE: #{digit_second} #{digit_third}"
-        digit_done = true
-        digit_at_index = digit_second
-      end
-      
     end
     
   # decrease
   else
-        
-    puts "decrease"
     digit_at_index -= jump unless digit_at_index == 0
     
     if digit_at_index == 0 && digit_second == 0
       digit_done = true
     else
       digit_done = false
-      
-      if changed_directions == 4
-        digit_done = true
-        digit_at_index = digit_third
-      end
-      
     end
 
   end
+  
+  # if we flip-flop back and forth between finding our number is too high
+  # or too low then our number is probably in the middle
+  # 4 swaps will leave us with the lower number
+  # which then the next digit will need to increase
+  # TODO: if this flops the wrong way, the algorithm breaks.
+  if changed_directions == 4
+    digit_done = true
+    digit_at_index = digit_second
+  end
+  
 
   # substitute our done digit in place
   current_number_string = current_number.to_s
-  
   current_number_array = current_number_string.chars.to_a
   current_number_array[index] = digit_at_index
   current_number = current_number_array.join.to_i
-  
-  
-  #dig_hist.push digit_at_index
-  
+
+  # move on to the next digit
   if digit_done
-    
-    # move on to the next digit
     index += 1
     digit_at_index = current_number.to_s[index..index].to_i
     digit_ceiling = 10
@@ -141,24 +130,20 @@ while (index <= starting_number.to_s.length - 1)
 
     digit_second = 0
     digit_third = 0
-
-    
-    #dig_hist = [10]
   end
-  
 
-puts "current_number:#{current_number} index:#{index} digit_at_index:#{digit_at_index} digit_second:#{digit_second} digit_third:#{digit_third} jump:#{jump} chdir:#{changed_directions}"
+  # debug status text
+  #puts "current_number:#{current_number} index:#{index} digit_at_index:#{digit_at_index} digit_second:#{digit_second} digit_third:#{digit_third} jump:#{jump} chdir:#{changed_directions}"
 
+  puts current_number
 
-# bug avoid
-if current_number.to_s.length != starting_number.to_s.length
- exit
-end
+  # bug avoid
+  if current_number.to_s.length != starting_number.to_s.length
+   exit
+  end
 
-sleep 0.1
-  
-  # move one decimal place if we can't increment
-  
+  # not needed but useful for watching how the algorithm works
+  sleep 0.1
   
 end
 
