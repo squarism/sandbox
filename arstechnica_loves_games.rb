ffd9fda3b05cce532608409542c189b6febe6140
Chris Dillon <milkfilk@gmail.com>
2011-01-30 21:44:49 -0500

    Dumping html doc with base64.  Started parsing titles, have some working, need more examples.  Storing doc as zlib compressed failed.  Msgpack failed.  Major cleanup and refactoring ahead after functional.

--- arstechnica_loves_games.rb
+++ arstechnica_loves_games.rb
@@ -9,13 +9,15 @@ require 'net/http'
 require 'uri'
 require 'date'
 require 'yaml'
+require 'base64'
+require 'msgpack'
 
 class Scraper
   attr_accessor :number_of_results
   attr_accessor :reviews        # hash of parsed reviews
-  attr_accessor :starting_url
-  attr_accessor :page
-  attr_accessor :test_mode
+  attr_accessor :starting_url   # static google results URL
+  attr_accessor :page           # google result page iterator number
+  attr_accessor :test_mode      # uses cache .yaml file for testing instead of hitting google
   
   def initialize
     self.number_of_results = 0
@@ -28,19 +30,18 @@ class Scraper
   
   def scrape
     
+    # test mode speeds up development because you don't have to wait 5 minutes to scrape ars
     if self.test_mode
-      ## Skip hammering google by loading from a cache file
-      dump_file = File.open("ars_dump.yaml")
-      #reviews = Array.new
+      # Skip hammering google by loading from a cache file
+      dump_file = File.open(File.dirname(__FILE__) + "/ars_dump.yaml")
       YAML::load_documents(dump_file) do |doc|
-        #puts doc[:title]
         self.reviews << doc
       end
       dump_file.close
       
+      # skips all rest of scrape
       return
     end
-    
     
     # get the google results for the current page
     @doc = Nokogiri::HTML(open(self.search_url))
@@ -60,19 +61,38 @@ class Scraper
         puts $!
       end
       
-      reviews << { :title => @title, :link => @link, :date => @date }
+      @nokogiri_doc = Nokogiri::HTML(open(@link))
+      
+      # gzip fail
+      # @serialized_doc = @nokogiri_doc.serialize({:encoding => 'utf-8', :save_with => 0})
+      # @compressed_doc = Zlib::Deflate.deflate(@serialized_doc, Zlib::DEFAULT_COMPRESSION)
+      # reviews << { :title => @title, :link => @link, :date => @date, :doc => @compressed_doc }
+      
+      # base64 fail
+      @encoded_doc = Base64::encode64(@nokogiri_doc.to_s)
+      self.reviews << { :title => @title, :link => @link, :date => @date, :doc => @encoded_doc }
+      
+      #@msg = @nokogiri_doc.to_s.to_msgpack
+      #reviews << { :title => @title, :link => @link, :date => @date, :doc => @msg }
+
       puts @title
     end
     
+    # if we run out of google results size will be less than 10 (full page of links)
     if @doc.css('li.g').size >= 10
       self.page += 1
       puts "Sleeping before page: #{self.page} ..."
-      sleep 10
-      scrape
+      
+      # sleep to avoid hammering
+      #sleep 3
+      
+      #scrape
     end
     
-    File.open("ars_dump.yaml", "w") do |file|
-      s.reviews.each do |hash|
+    # will get a 503 if you hammer google, so we'll cache to file
+    # this won't fire because of the return near top
+    File.open(File.dirname(__FILE__) + "/ars_dump.yaml", "w") do |file|
+      self.reviews.each do |hash|
         file.puts YAML::dump(hash)
       end
     end
@@ -96,14 +116,31 @@ class Scraper
       reviews[fp] = nil
     end
     reviews.compact!
-  end 
+  end
+  
+  def to_s
+    puts "#{self.title} - #{self.link}"
+  end
     
 end
 
-# will get a 503 if you hammer google, so we'll cache to file
+
+def deserialize(element)
+  # zstream = Zlib::Inflate.new
+  # buf = zstream.inflate(element[:doc])
+  # deserialized_doc = Nokogiri::HTML.parse(buf)
+
+  
+  deserialized_doc = Nokogiri::HTML.parse(Base64::decode64(element[:doc]))
+end
+
+
+
 s = Scraper.new
 s.scrape
 
+# Pages of results: 
+# Expected size: 
 puts "before trim: #{s.reviews.size}"
 
 # trim out forum posts
@@ -127,17 +164,71 @@ s.reviews = s.reviews.uniq { |e| e[:title] }
 
 puts "after trim: #{s.reviews.size}"
 
+# test unique title trim
+# s.reviews.each do |r|
+#   if r[:title][/Reach/]
+#     puts r
+#   end
+# end
 
-
+review_style_1 = ""
 s.reviews.each do |r|
-  if r[:title][/Reach/]
-    puts r
+  if r[:title][/^Let them/]
+    review_style_1 = r
   end
 end
+#puts review_style_1[:link]
+puts review_style_1.size
 
+# gzip
+#doc = deserialize(review_style_1)
+
+# base64
+
+#puts doc
+
+
+#puts unpacked_doc = Nokogiri::HTML.parse(MessagePack.unpack review_style_1[:doc])
+
+
+#puts doc.css('.news-item-figure') #.css('th') #[1].text
 
 
 s.reviews.each do |r|
-  puts "#{r[:title]} - #{r[:link]}"
+  doc = Nokogiri::HTML.parse(Base64::decode64(r[:doc]))
+  #doc = Nokogiri::HTML::parse r[:doc]
+  puts r[:link]
+  
+  # style -1
+  #if !doc.css('.news-item-figure').css('th')[1].nil?
+  #  print "STYLE 1: "
+  #  puts doc.css('.news-item-figure').css('th')[1].text
+  #end
+    
+  # style 1 (table with heading, bleh)
+  if !doc.css('tbody').css('th')[1].nil?
+    print "STYLE 1: "
+    puts doc.css('tbody').css('th')[1].text
+  end
+  
+  # style 2 (nice game-info div)
+  if !doc.css('.game-info').css('h3')[0].nil?
+    print "STYLE 2: "
+    puts doc.css('.game-info').css('h3')[0].text
+  end
+  
+  # style 3 (nothing really, title detection)
+  # http://arstechnica.com/gaming/news/2007/02/7048.ars
+  # Game Review: WiiPlay (Wii)
+  if doc.css('title').text[/^Game Review:/]
+    print "STYLE 3: "
+    puts doc.css('title').text.split(":")[1].strip
+  end
+  
 end
+
+
+# s.reviews.each do |r|
+#   puts "#{r[:title]} - #{r[:link]}"
+# end
 
